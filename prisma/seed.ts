979c5d4f3350bb2e822fba244a9fdd43e2fcdb3e
Omoshola <heylehgbedey@gmail.com>
2026-03-16 05:47:20 -0700

    feat: add @faker-js/faker for generating dynamic post content and update seed script to create posts for seeded users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,9 @@ import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 import bcrypt from "bcrypt";
+import { rawUsers } from "@/lib/data";
+import { fakerEN as faker } from "@faker-js/faker";
+
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -11,130 +14,21 @@ const pool = new Pool({
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
-const rawUsers = [
-  {
-    name: "Alice Johnson",
-    email: "alice@example.com",
-    username: "alicejohnson",
-    password: "password123",
-  },
-  {
-    name: "Brian Smith",
-    email: "brian@example.com",
-    username: "briansmith",
-    password: "password123",
-  },
-  {
-    name: "Cynthia Brown",
-    email: "cynthia@example.com",
-    username: "cynthiabrown",
-    password: "password123",
-  },
-  {
-    name: "David Wilson",
-    email: "david@example.com",
-    username: "davidwilson",
-    password: "password123",
-  },
-  {
-    name: "Ella Davis",
-    email: "ella@example.com",
-    username: "elladavis",
-    password: "password123",
-  },
-  {
-    name: "Frank Miller",
-    email: "frank@example.com",
-    username: "frankmiller",
-    password: "password123",
-  },
-  {
-    name: "Grace Moore",
-    email: "grace@example.com",
-    username: "gracemoore",
-    password: "password123",
-  },
-  {
-    name: "Henry Taylor",
-    email: "henry@example.com",
-    username: "henrytaylor",
-    password: "password123",
-  },
-  {
-    name: "Isabella Anderson",
-    email: "isabella@example.com",
-    username: "isabellaanderson",
-    password: "password123",
-  },
-  {
-    name: "James Thomas",
-    email: "james@example.com",
-    username: "jamesthomas",
-    password: "password123",
-  },
-  {
-    name: "Karen Jackson",
-    email: "karen@example.com",
-    username: "karenjackson",
-    password: "password123",
-  },
-  {
-    name: "Liam White",
-    email: "liam@example.com",
-    username: "liamwhite",
-    password: "password123",
-  },
-  {
-    name: "Mia Harris",
-    email: "mia@example.com",
-    username: "miaharris",
-    password: "password123",
-  },
-  {
-    name: "Noah Martin",
-    email: "noah@example.com",
-    username: "noahmartin",
-    password: "password123",
-  },
-  {
-    name: "Olivia Thompson",
-    email: "olivia@example.com",
-    username: "oliviathompson",
-    password: "password123",
-  },
-  {
-    name: "Paul Garcia",
-    email: "paul@example.com",
-    username: "paulgarcia",
-    password: "password123",
-  },
-  {
-    name: "Queen Lee",
-    email: "queen@example.com",
-    username: "queenlee",
-    password: "password123",
-  },
-  {
-    name: "Ryan Martinez",
-    email: "ryan@example.com",
-    username: "ryanmartinez",
-    password: "password123",
-  },
-  {
-    name: "Sophia Robinson",
-    email: "sophia@example.com",
-    username: "sophiarobinson",
-    password: "password123",
-  },
-  {
-    name: "Daniel Walker",
-    email: "daniel@example.com",
-    username: "danielwalker",
-    password: "password123",
-  },
-];
+function generatePosts(users: { id: string }[]) {
+  const numberOfPostByUser = 2
+  return users.flatMap((user) => {
+    return Array.from({length: numberOfPostByUser}).map(() => ({
+      content: faker.hacker.phrase(),
+      authorId: user.id,
+    }));
+  });
+}
 
 async function main() {
+
+  await prisma.user.deleteMany();
+  await prisma.post.deleteMany();
+
   const seededUsers = await Promise.all(
     rawUsers.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
@@ -148,9 +42,37 @@ async function main() {
     }),
   );
 
-  await prisma.user.createMany({
+  const users = await prisma.user.createManyAndReturn({
     data: seededUsers,
     skipDuplicates: true,
+    select: {
+      id: true
+    }
+  });
+
+  //  const users = await prisma.user.createMany({
+  //    data: seededUsers,
+  //    skipDuplicates: true,
+  //  });
+
+  // console.log(users)
+
+  // fetch the users to get their ids for seeding related data
+  // const users = await prisma.user.findMany({
+  //   where: {
+  //     email: {
+  //       in: rawUsers.map((user) => user.email),
+  //     },
+  //   },
+  //   select: {
+  //     id: true,
+  //   },
+  // });
+
+  const post = generatePosts(users);
+
+  await prisma.post.createMany({
+    data: post,
   });
 }
 
